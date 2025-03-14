d6e504e49e2630f2effba659ddaffc7e0e35b4e6
Wesley Sangalli <wesley.sangalli@gmail.com>
2025-03-13 21:01:58 -0300

    update config

--- modules/database/rds-postgres.tf
+++ modules/database/rds-postgres.tf
@@ -8,9 +8,23 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   publicly_accessible  = true
   skip_final_snapshot  = true
-  parameter_group_name = "default.postgres17"
+  parameter_group_name = aws_db_parameter_group.postgres17_params.name
   db_subnet_group_name = aws_db_subnet_group.default.name
+
+  availability_zone = local.availability_zone
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
+
+resource "aws_db_parameter_group" "postgres17_params" {
+  name   = "fiap-fastfood-db-params"
+  family = "postgres17"
+
+  # parameter {
+  #   name  = "rds.force_ssl"
+  #   value = "0"
+  # }
+}
+
 
 resource "aws_db_subnet_group" "default" {
   name       = "fiap-fastfood-db-subnet-group"
