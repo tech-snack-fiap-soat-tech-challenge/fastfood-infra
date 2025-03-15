79d084e32a323c6e1a057015912ef8c9f90f8d82
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-03-15 18:15:09 -0300

    feat: add EKS cluster and node group configuration with necessary variables

--- modules/compute/locals.tf
+++ modules/compute/locals.tf
@@ -1,8 +1,7 @@
 locals {
     application_name = "fiap-fastfood"
-    kubernetes_version = "1.31"
+    kubernetes_version = "1.28"
     instance_type = "t3.micro"
-    node_role_arn = "arn:aws:iam::467897245441:role/LabRole"
-
+    # node_role_arn = "arn:aws:iam::467897245441:role/LabRole"
     subnet_ids = var.subnet_ids
 }
--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,3 +2,8 @@ variable "subnet_ids" {
   type = list(string)
   description = "The list of subnet IDs in which to place the EC2 instances"
 }
+
+variable "node_role_arn" {
+  type        = string
+  description = "ARN of the EKS Node Group Role"
+}
