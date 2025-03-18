cb2743953028b59e59b28082172d3bd21efc5aba
Wesley Sangalli <wesley.sangalli@gmail.com>
2025-03-18 20:40:22 -0300

    add lambda

--- modules/computing/ec2.tf
+++ modules/computing/ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_eks_cluster" "fiap_fastfood" {
   name     = "fiap-fastfood"
-  role_arn = local.node_role_arn
+  role_arn = local.lab_role_arn
 
   vpc_config {
     subnet_ids = local.subnet_ids
@@ -21,7 +21,7 @@ resource "aws_eks_addon" "metrics_server" {
 resource "aws_eks_node_group" "default_ng1" {
   cluster_name    = aws_eks_cluster.fiap_fastfood.name
   node_group_name = "default-ng1"
-  node_role_arn   = local.node_role_arn
+  node_role_arn   = local.lab_role_arn
   subnet_ids      = aws_eks_cluster.fiap_fastfood.vpc_config[0].subnet_ids
 
   scaling_config {
--- modules/computing/locals.tf
+++ modules/computing/locals.tf
@@ -1,8 +1,8 @@
 locals {
-    application_name = "fiap-fastfood"
-    kubernetes_version = "1.31"
-    instance_type = "t3.micro"
-    node_role_arn = "arn:aws:iam::467897245441:role/LabRole"
+  application_name   = "fiap-fastfood"
+  kubernetes_version = "1.31"
+  instance_type      = "t3.micro"
+  lab_role_arn       = "arn:aws:iam::467897245441:role/LabRole"
 
-    subnet_ids = var.subnet_ids
+  subnet_ids = var.subnet_ids
 }
