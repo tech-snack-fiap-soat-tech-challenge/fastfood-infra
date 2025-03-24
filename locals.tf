9461302820af1363fbb2bdc57579af239fa09f62
lidia-freitas <lidiafreitas.me@gmail.com>
2025-03-24 02:34:21 -0300

    feat: Enhance API Gateway configuration with Cognito integration and new endpoints

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
-  application_name = "fiap-fastfood"
-  lab_role_arn     = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
+  application_name  = "fiap-fastfood"
+  lab_role_arn      = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
+  auth_redirect_uri = "https://${local.application_name}.com/callback"
 }
