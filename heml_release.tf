bfc79e11fd3159d7c006046e5d82f89178ad7a8d
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-03-24 19:18:45 -0300

    feat: Configure EKS cluster with Helm provider and install Metrics Server

---
+++ heml_release.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "metrics_server" {
+  provider   = helm.eks
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+
+  set {
+    name  = "args[0]"
+    value = "--kubelet-insecure-tls"
+  }
+
+  depends_on = [module.compute]
+}
---
+++ providers.tf
@@ -0,0 +1,25 @@
+data "aws_eks_cluster" "fiap_fastfood" {
+  name       = module.compute.cluster_name
+  depends_on = [module.compute]
+}
+
+data "aws_eks_cluster_auth" "fiap_fastfood" {
+  name       = module.compute.cluster_name
+  depends_on = [module.compute]
+}
+
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.fiap_fastfood.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.fiap_fastfood.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.fiap_fastfood.token
+}
+
+provider "helm" {
+  alias = "eks"
+  kubernetes {
+    host                   = data.aws_eks_cluster.fiap_fastfood.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.fiap_fastfood.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.fiap_fastfood.token
+  }
+}
