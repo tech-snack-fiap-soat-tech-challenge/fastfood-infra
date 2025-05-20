1dab58c880dcbc0dbce38a3f61682bd4b954178c
Wesley Sangalli <wesley.sangalli@gmail.com>
2025-05-20 20:23:47 -0300

    feat: add ecr repositories

--- modules/compute/locals.tf
+++ modules/compute/locals.tf
@@ -3,4 +3,11 @@ locals {
   kubernetes_version = "1.32"
   instance_type      = "t3.micro"
   subnet_ids         = var.subnet_ids
+
+  ecr_repositories = toset([
+    "fastfood-order-service",
+    "fastfood-product-service",
+    "fastfood-customer-service",
+    "fastfood-checkout-service"
+  ])
 }
